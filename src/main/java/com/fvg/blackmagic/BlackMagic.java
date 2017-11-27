acc944c0d27cedde506990f87b52ff024ee35924
FlamingVikingGoat <the.epic.scientist5@gmail.com>
2017-11-26 23:06:53 -0600

    NEVER AGAIN

---
+++ src/main/java/com/fvg/blackmagic/BlackMagic.java
@@ -0,0 +1,16 @@
+package com.fvg.blackmagic;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = BlackMagic.MODID, version = BlackMagic.VERSION)
+public class BlackMagic {
+    public static final String MODID = "blackMagic";
+    public static final String VERSION = "1.0.0";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+}
