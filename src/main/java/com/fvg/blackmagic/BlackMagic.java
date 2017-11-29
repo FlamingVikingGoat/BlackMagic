f428af3eb48cbc1409fe532faaedeeb5b5793a49
FlamingVikingGoat <the.epic.scientist5@gmail.com>
2017-11-28 20:38:26 -0600

    Adjusted core

--- src/main/java/com/fvg/blackmagic/BlackMagic.java
+++ src/main/java/com/fvg/blackmagic/BlackMagic.java
@@ -3,6 +3,8 @@ package com.fvg.blackmagic;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import static com.fvg.blackmagic.BootsOfSpeed.bootsOfSpeed;
@@ -14,11 +16,21 @@ public class BlackMagic {
     public static final String VERSION = "1.0.0";
 
     @EventHandler
-    public void init(FMLInitializationEvent event){
+    public  static void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @EventHandler
+    public static void init(FMLInitializationEvent event){
         SpawnBlock spawnBlock = new SpawnBlock();
         GameRegistry.registerBlock(spawnBlock, "spawnBlock");
         bootsOfSpeed = new BootsOfSpeed(demonArmor, 0, 3);
         GameRegistry.registerItem(bootsOfSpeed, "bootsOfSpeed");
+
+    }
+
+    @EventHandler
+    public static void postInit(FMLPostInitializationEvent event){
 
     }
 }
