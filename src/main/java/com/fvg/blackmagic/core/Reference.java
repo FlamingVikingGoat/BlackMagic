c8bbcc4ac51e963e0d8eafbc4f4e471cc6eaf845
FlamingVikingGoat <the.epic.scientist5@gmail.com>
2017-12-03 21:52:30 -0600

    Changed Folders

--- src/main/java/com/fvg/blackmagic/core/Reference.java
+++
@@ -1,8 +0,0 @@
-package com.fvg.blackmagic.core;
-
-public class Reference {
-    public static final String MODID = "blackmagic";
-    public static final String VERSION = "1.0";
-    public static final String SERVER_PROXY_CLASS = "com.fvg.blackmagic.proxy.CommonProxy";
-    public static final String CLIENT_PROXY_CLASS = "com.fvg.blackmagic.proxy.ClientProxy";
-}
--- src/main/java/com/fvg/blackmagic/items/gear/IBlackMagicGear.java
+++
@@ -1,9 +0,0 @@
-package com.fvg.blackmagic.items.gear;
-
-import net.minecraft.entity.player.EntityPlayer;
-
-public interface IBlackMagicGear {
-
-    void addPositiveEffect(EntityPlayer player);
-
-}
